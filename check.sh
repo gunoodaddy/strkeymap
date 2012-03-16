b80cdb5d15129c001aac09ac742326eb65217944
Eunhyuk Kim <gunoodaddy@gmail.com>
2012-03-16 11:23:29 +0900

    delete file..

--- check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf result.valgrind*
-#valgrind -v --tool=memcheck --leak-check=yes  --show-reachable=yes  --log-file=result.valgrind tester
-valgrind -v --tool=memcheck --leak-check=yes  --show-reachable=yes tester
